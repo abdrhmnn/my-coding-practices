f941b7d2eb0120e6e86499ec38ff319210105dde
Abdurrahman <Abdrhmn.gh@gmail.com>
2022-09-14 18:27:40 +0700

    belajar tailwind

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	// ** maksudnya apapun folder nya
+	// * maksudnya apapun nama file nya
+	content: ["./src/**/*.{html,js}", "./index.html"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
